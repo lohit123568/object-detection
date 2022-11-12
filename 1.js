c2255914e7e71a1b51432a0347d55c7200622b4e
lohit123568 <87901963+lohit123568@users.noreply.github.com>
2022-11-12 15:02:52 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,15 @@
+function one() {
+    window.location("2.html")
+}
+function two() {
+    window.location("3.html")
+}
+function three() {
+    window.location("4.html")
+}
+function four() {
+    window.location("5.html")
+}
+function five() {
+    window.location("6.html")
+}
